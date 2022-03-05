10b2b4ca7015fe5dee934d9ecd7d5c503795aa60
EdizzyGuy <ld18821@bristol.ac.uk>
2022-03-05 14:18:10 +0000

    Forgot to commit when changes were made, so I forgot what changes were made

--- Euler.py
+++ Euler.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def euler_step(dx_dt, initial_point, step_size):
@@ -9,34 +10,77 @@ def euler_step(dx_dt, initial_point, step_size):
     return new_point
 
 
-def solve_to(dx_dt, initial_point, deltat_max=0.001, time_interval=[0, 1]):
+def RK4_step(dx_dt, initial_point, step_size):
+    """ Will implement an increment in the independent variable of size step_size and find
+    new value of dependent variable based on derivative of a system of equations (dx_dt)
+    using the classic Runge-Kutta """
+# initialise values of k
+    k1 = dx_dt(initial_point)
+    k2 = dx_dt(initial_point + step_size*k1/2)
+    k3 = dx_dt(initial_point + step_size*k2/2)
+    k4 = dx_dt(initial_point + step_size*k3)
+
+    new_point = initial_point + step_size/6 *(k1 +2*k2 + 2*k3 + k4)
+    return new_point
+
+
+def solve_to(dx_dt, initial_point, deltat_max=0.001, time_interval=[0, 1], method='RK4'):
     """ Function will use Euler's method to solve a system of equations characterised by x
     with a maximum step size between subsequent x being deltat_max.
     Will solve ODE from time_interval[START, FINISH], where start is taken as argument in case the dynamical
     system is implicitly time dependent"""
 
-    num_steps = int(np.ceil((time_interval[1] - time_interval[0])/deltat_max))
-    steps = [deltat_max for i in range(num_steps - 1)]
-    steps.append(time_interval[1] - time_interval[0] - np.sum(steps))  # manually add last step
-
+# MAKE USER UNABLE TO ENTER ANY OTHER METHOD THAN A VALID ONE
 # TEST test to check if steps sum to time interval
-    current_euler = initial_point
+    valid_methods = ['Euler', 'RK4']
+    if method not in valid_methods:
+        # WHAT ERROR SHOULD I RAISE
+        # ALSO MAKE IT BREAK THE FUNCTION AND SUPERCEEDING FUNCTIONS
+        pass
+
+    current_step = initial_point
     current_time = time_interval[0]
-    for step in steps:
-        next_euler = euler_step(dx_dt, current_euler, step_size=step)
+    start = time.time()
+    i = 0
 
-        current_time += step
-        current_euler = next_euler
+    match method:
+        case 'Euler':
+            while time_interval[1] - current_time > deltat_max:
+                next_step = euler_step(dx_dt, current_step, step_size=deltat_max)
+
+                current_time += deltat_max
+                current_step = next_step
+                i += 1
+                if i == 10**5:
+                    end = time.time()
+                    elapsed = end - start
+                    print(elapsed)
+            min_step = time_interval[1] - current_time
+            last_step = euler_step(dx_dt, current_step, step_size=min_step)
+
+        case 'RK4':
+            while time_interval[1] - current_time > deltat_max:
+                next_step = RK4_step(dx_dt, current_step, step_size=deltat_max)
+
+                current_time += deltat_max
+                current_step = next_step
+                i += 1
+                if i == 10 ** 5:
+                    end = time.time()
+                    elapsed = end - start
+                    print(elapsed)
+            min_step = time_interval[1] - current_time
+            last_step = RK4_step(dx_dt, current_step, step_size=min_step)
 # NUMERICAL ISSUE : time does not add up to what it is meant to be exactly due to rounding errors
-    return current_euler
+    return last_step
 
 
-def solve_ode(dx_dt, initial_condition, time, deltat_max=0.001):
-    x = np.zeros(shape=(initial_condition.size, len(time)))
-    x[:, 0] = initial_condition
+def solve_ode(dx_dt, initial_condition, time, deltat_max=0.001, method='RK4'):
+    x = np.zeros(shape=(len(time), initial_condition.size))
+    x[0, :] = initial_condition
 
     for i in range(len(time)-1):
-        x[:, i+1] = solve_to(dx_dt, x[:, i], deltat_max, [time[i], time[i+1]])
+        x[i+1, :] = solve_to(dx_dt, x[i, :], deltat_max, [time[i], time[i+1]], method=method)
     return x
 
 
--- Question_1.py
+++ Question_1.py
@@ -1,27 +1,74 @@
 import Euler
 import numpy as np
+import matplotlib.pyplot as plt
 
 ''' TEST IDEAS
 solve ode to a certain point
-solve ode with multiple points in between'''
+solve ode with multiple points in between AND PRINT ERRORS'''
+
+'''Q1 : Solve ODE x_dot = x, with initial condition x(0) = 1.
+solve with different time steps'''
+
 
 def dx_dt(x):
     x_dot = x
     return x_dot
 
 
-print('we have started')
-time = np.linspace(0, 10)
-#print(time)
-initial_condition = np.array(1)
-result = Euler.solve_ode(dx_dt, initial_condition, time, deltat_max=0.00001)
-#print(result)
+time_steps = np.logspace(-4, 0, 42)
+# start at 10^-16 as this is the smallest number matlab can represent IS IT
+# -16 is too much will req 10^16 steps to complete a unit
+# end at 10^0 = 1 since largest timestep from 0 to 1 is 1.
 
-true_ans = np.exp(time)
+time = np.array([0, 1])
+initial_condition = np.array(1)
+true_ans = np.exp(time[1])
+errors_euler = np.zeros(shape=time_steps.shape)
+errors_RK4 = np.zeros(shape=time_steps.shape)
+
+i = 0
+for time_step in time_steps:
+    result_euler = Euler.solve_ode(dx_dt, initial_condition, time, deltat_max=time_step, method='Euler')
+    result_RK4 = Euler.solve_ode(dx_dt, initial_condition, time, deltat_max=time_step, method='RK4')
+
+    errors_euler[i] = result_euler[1][0] - true_ans
+    errors_RK4[i] = result_RK4[1][0] - true_ans
+    if i % 10 == 0:
+        print(i)
+    i += 1
+
+fig = plt.figure(figsize=(8, 6), dpi=100)
+fig.suptitle('Effectiveness of solve_ode function with different iterative methods')
+
+ax1 = plt.subplot(1, 2, 1)
+plt.loglog(time_steps, np.abs(errors_euler), color='blue', linewidth=2.5, linestyle='-')
+ax1.set_xlabel(r'Maximum step size')
+ax1.set_ylabel(r'Error in approximation to x(1)')
+ax1.set_title(r"Euler's method")
+plt.grid()
+
+ax2 = plt.subplot(1, 2, 2)
+plt.loglog(time_steps, np.abs(errors_RK4), color='blue', linewidth=2.5, linestyle='-')
+ax2.set_xlabel(r'Maximum step size')
+ax2.set_ylabel(r'Error in approximation to x(1)')
+# maybe doesnt need y axis label
+ax2.set_title(r"classic Runge-Kutta method")
+plt.grid()
+
+plt.show()
+while True:
+    print('eat shit')
+
+
+'''
 error = result - true_ans
-print(error)
-print(np.average(error))
+print('Individual errors are : ', error)
+print('Average error is :', np.average(error))
 # HAS REDUCING ACCURACY AS ESTIMATES DIVERGE FROM INITIAL POINT
+'''
+'''Produce a (nicely formatted) plot with double logarithmic scale showing how 
+the error depends on the size of the timestep Δt'''
+# DOUBLE LOGARITHMIC SCALE IS LOG LOG GRAPH
 
 '''
 WORKS WITH 2 TIMES (time = [0, 1])
